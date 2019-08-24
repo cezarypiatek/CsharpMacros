a84ed3126cb10a4811caae0633f413738403b1c5
cezarypiatek <cezarypiatek@live.com>
2019-08-24 22:48:37 +0200

    Add derived macro

---
+++ src/CsharpMacros/Macros/DerivedMacro.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis.FindSymbols;
+
+namespace CsharpMacros.Macros
+{
+    class DerivedMacro:ICsharpMacro
+    {
+        public IEnumerable<Dictionary<string, string>> ExecuteMacro(string param, ICsharpMacroContext context)
+        {
+            var typeInfo = TypeHelper.GetTypeInfo(param, context);
+            var derived = SymbolFinder.FindDerivedClassesAsync(typeInfo.Symbol, context.Solution).GetAwaiter().GetResult();
+            foreach (var derivedType in derived)
+            {
+                yield return new Dictionary<string, string>()
+                {
+                    ["name"] = derivedType.GetFullGenericName(),
+                    ["based"] = typeInfo.Symbol.Name
+                };
+            }
+        }
+    }
+}
