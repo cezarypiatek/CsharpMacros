5866e06e82efd22801b1837097b8c675ae94dfc6
Cezary Piątek <cezarypiatek@live.com>
2019-11-06 17:39:42 +0100

    Introduce common fixture for macro tests

--- src/CsharpMacros.Test/Macros/Range/RangeMacroTest.cs
+++ src/CsharpMacros.Test/Macros/Range/RangeMacroTest.cs
@@ -1,24 +1,13 @@
-﻿using CleanCoder;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CodeFixes;
-using NUnit.Framework;
-using RoslynTestKit;
+﻿using NUnit.Framework;
 
 namespace CsharpMacros.Test.Macros.Range
 {
-    public class RangeMacroTest : CodeFixTestFixture
+    public class RangeMacroTest : MacroTestFixture
     {
         [Test]
         public void should_be_able_to_execute_macro_for_range()
         {
-            TestCodeFix(TestCases._001_SimpleRange, TestCases._001_SimpleRange_FIXED, MacroCodeAnalyzer.Rule);
-        }
-
-
-        protected override string LanguageName => LanguageNames.CSharp;
-        protected override CodeFixProvider CreateProvider()
-        {
-            return new CsharpMacrosCodeFixProvider();
+            VerifyMacro(TestCases._001_SimpleRange, TestCases._001_SimpleRange_FIXED);
         }
     }
 }
