1794b6f7f3d47839854d42a3301ef138b4f2ef18
Mostafa Ali <engabdomostafa@gmail.com>
2019-11-19 14:08:17 -0700

    Add step parameter for range macro

--- src/CsharpMacros.Test/Macros/Range/RangeMacroTest.cs
+++ src/CsharpMacros.Test/Macros/Range/RangeMacroTest.cs
@@ -8,6 +8,7 @@ namespace CsharpMacros.Test.Macros.Range
         public void should_be_able_to_execute_macro_for_range()
         {
             VerifyMacro(TestCases._001_SimpleRange, TestCases._001_SimpleRange_FIXED);
+            VerifyMacro(TestCases._002_SimpleRangeStep, TestCases._002_SimpleRangeStep_FIXED);
         }
     }
 }
